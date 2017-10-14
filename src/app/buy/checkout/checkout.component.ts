f9bd13fd83a2bcb0dea92f34f798bcd5dda6cc6d
Eugenio Valeiras <eugenio.valeiras@gmail.com>
2017-10-13 21:13:14 -0300

    TD Form

--- src/app/buy/checkout/checkout.component.ts
+++ src/app/buy/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -10,6 +11,10 @@ export class CheckoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submitCheckout(form: NgForm) {
+    console.log(form);
   }
 
 }
