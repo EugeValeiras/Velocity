efba2a69608701d479207cc58bc8b90fce26ff7d
Eugenio Valeiras <eugenio.valeiras@gmail.com>
2017-10-16 12:01:35 -0300

    Add E2E tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,80 @@
-import { AppPage } from './app.po';
+import {AppPage} from './app.po';
+import {browser} from 'protractor';
+import {Key} from 'selenium-webdriver';
 
 describe('velocity App', () => {
   let page: AppPage;
+  let randomProductUrl;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display Products container', () => {
+    page.navigateToShop();
+    expect(page.getProductsContainer().isPresent());
   });
+
+  it('should view a Products', () => {
+    page.navigateToShop();
+    const firstProductContainer = page.getFirstProductContainer();
+    const viewButton = page.getFirstViewProductButton();
+    expect(viewButton.isPresent());
+    expect(viewButton.getText()).toEqual('View');
+    viewButton.click();
+    expect(browser.getCurrentUrl()).toContain('/shopping/product');
+    browser.getCurrentUrl()
+      .then(url => randomProductUrl = url);
+  });
+
+  it('should Buy a Products', () => {
+    browser.get(randomProductUrl);
+    const buyButton = page.getProductBuyButton();
+    expect(buyButton.isPresent());
+    expect(buyButton.getText()).toEqual('Buy');
+    buyButton.click();
+    const cartCounter = page.getCartCounter();
+    expect(cartCounter.getText()).toEqual('1');
+  });
+
+  it('should show name error message ONLY on touched', () => {
+    page.navigateToCheckoutForm();
+    const nameInput = page.getNameInput();
+    const nameErrorMessage = page.getNameErrorMessage()
+    expect(nameErrorMessage.isPresent()).toEqual(false);
+    nameInput.click();
+    expect(nameErrorMessage.isPresent()).toEqual(false);
+    nameInput.sendKeys(Key.TAB);
+    expect(nameErrorMessage.isPresent()).toEqual(true);
+  });
+
+
+  it('should complete checkout form', () => {
+    page.navigateToCheckoutForm();
+    const nameInput = page.getNameInput();
+    const lastNameInput = page.getLastNameInput();
+    const emailInput = page.getEmailInput();
+    const maleInput = page.getMaleInput();
+    const addressInput = page.getAddressInput();
+    const zipCodeInput = page.getZipCodeInput();
+    const butButton = page.getCheckoutBuyButton();
+
+    // Complete Data
+    nameInput.sendKeys('Eugenio');
+    expect(butButton.getAttribute('disabled')).toEqual('true');
+    lastNameInput.sendKeys('Valeiras');
+    expect(butButton.getAttribute('disabled')).toEqual('true');
+    emailInput.sendKeys('eugenio.valeiras@gmail.com');
+    expect(butButton.getAttribute('disabled')).toEqual('true');
+    maleInput.click();
+    expect(butButton.getAttribute('disabled')).toEqual('true');
+    addressInput.sendKeys('My Address 1234');
+    expect(butButton.getAttribute('disabled')).toEqual('true');
+    zipCodeInput.sendKeys(12349);
+    expect(butButton.getAttribute('disabled')).toEqual(null);
+
+    butButton.click();
+  });
+
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,63 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateToShop() {
+    return browser.get('/shopping/shop');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  navigateToProduct(id) {
+    return browser.get(`/shopping/product/${id}`);
+  }
+
+  navigateToCheckoutForm() {
+    return browser.get(`/buy/checkout`);
+  }
+
+  getProductsContainer() {
+    return element(by.className('productContainer'));
+  }
+
+  getFirstViewProductButton() {
+    return element(by.css('button'));
+  }
+
+  getFirstProductContainer() {
+    return element(by.className('productItem'));
+  }
+
+  getProductBuyButton() {
+    return element(by.css('button'));
+  }
+
+  getCartCounter() {
+    return element(by.id('cartCounter'));
+  }
+
+
+  // Checkout Page
+  getNameInput() {
+    return element(by.css('input[name="name"]'));
+  }
+  getNameErrorMessage() {
+    return element(by.id('nameErrorMessage'));
+  }
+
+  getLastNameInput() {
+    return element(by.css('input[name="lastName"]'));
+  }
+  getEmailInput() {
+    return element(by.css('input[name="email"]'));
+  }
+  getMaleInput() {
+    return element(by.css('#male'));
+  }
+  getAddressInput() {
+    return element(by.css('input[name="address"]'));
+  }
+  getZipCodeInput() {
+    return element(by.css('input[name="zipCode"]'));
+  }
+  getCheckoutBuyButton() {
+    return element(by.css('button'));
   }
 }
